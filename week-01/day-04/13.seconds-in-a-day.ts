57e90d527000a04296b8de772e5d6d861457cfb0
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-06-27 12:42:27 +0200

    Script excercise commit 13.seconds-in-a-day

---
+++ week-01/day-04/13.seconds-in-a-day.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+console.log( 24*60*60 - ( currentHours*60*60 + currentMinutes*60 + currentSeconds ) );
