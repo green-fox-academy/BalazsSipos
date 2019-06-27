74f1a9210d2b05a2674e788ca0e3e46d562f9bad
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-06-27 14:13:44 +0200

    Script excercise commit 18.parametric-average

---
+++ week-01/day-04/18.parametric-average.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+let number: number = 5
+let sum: number = 0;
+let count: number = 0;
+
+for ( let i:number = 1;i<=number;i++) {
+    sum += i;
+    count++;
+}
+console.log("Sum: " + sum + ", Average: " + sum/count);
