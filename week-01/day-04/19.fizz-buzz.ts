c6285110d0fbb5898444fa445efe6dbaa22b94ee
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-06-27 14:21:19 +0200

    Script excercise commit 19.fizz-buzz

---
+++ week-01/day-04/19.fizz-buzz.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+for ( let i:number = 1;i<=100;i++) {
+    
+    if ( 0 == i % 3 && 0 == i % 5 ) {
+        console.log("FizzBuzz");
+    } else if ( 0 == i % 3 && 0 != i % 5 ) {
+        console.log("Fizz");
+    } else if ( 0 != i % 3 && 0 == i % 5 ) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
