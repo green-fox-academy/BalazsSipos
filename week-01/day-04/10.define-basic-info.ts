4c19d60cebc76ba0dfc5c2699e4f8f3aca8e623c
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-06-27 12:17:38 +0200

    Script excercise commit

---
+++ week-01/day-04/10.define-basic-info.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let myName: string = 'Balázs Sipos';
+console.log(myName);
+// Your age as a number
+let myAge: number = 37;
+console.log(myAge);
+// Your height in meters as a number
+let myHeight: number = 1.8;
+console.log(myHeight);
+// Whether you are married or not as a boolean
+let amIMarried: boolean = true;
+console.log(amIMarried);
---
+++ week-01/day-04/11.variable-mutation.ts
@@ -0,0 +1,46 @@
+'use strict';
+
+let a: number = 3;
+// make it bigger by 10
+
+console.log(a);
+
+let b: number = 100;
+// make it smaller by 7
+
+console.log(b);
+
+let c: number = 44;
+// double c's value
+
+console.log(c);
+
+let d: number = 125;
+// divide d's value by 5
+
+console.log(d);
+
+let e: number = 8;
+// what's the cube of e's value?
+
+console.log(e);
+
+let f1: number = 123;
+let f2: number = 345;
+// tell if f1 is bigger than f2 (as a boolean)
+
+let g1: number = 350;
+let g2: number = 200;
+// tell if the double of g2 is bigger than g1 (pras a boolean)
+
+let h: number = 1357988018575474;
+// tell if 11 is a divisor of h (as a boolean)
+
+let i1: number = 10;
+let i2: number = 3;
+// tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+
+let j: number = 1521;
+// tell if j is divisible by 3 or 5 (as a boolean)
+
+console.log(k);
