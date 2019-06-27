aa46da3cdeb620ff8b9693658e51b2c022e13d59
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-06-27 15:47:48 +0200

    Script excercise commit 25.draw-chess-table

---
+++ week-01/day-04/25.draw-chess-table.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+for ( let i:number = 1;i<=8;i++ ) {
+    let collector:string = '';
+    for ( let j:number = 1;j<=8;j++ ) {
+        if ( ( 0 == i % 2 && 0 == j % 2 ) || ( 0 != i % 2 && 0 != j % 2 ) ) {
+            collector += '%';
+        } else {
+            collector += ' ';
+        }
+    }
+    console.log(collector);
+}
