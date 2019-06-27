2b4994ed6cf3979077b97f2e5b43a2a767bb0a33
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-06-27 12:37:15 +0200

    Script excercise commit till 12.cuboid

---
+++ week-01/day-04/12.cuboid.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+let a: number = 10;
+let surface: number = a ** 2 * 6;
+console.log(surface);
+// Surface Area: 600
+
+let volume: number = a ** 3;
+console.log(volume);
+// Volume: 1000
