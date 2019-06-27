1be13c29bf8b1eeda5b4554aee501d856f254841
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-06-27 14:43:36 +0200

    Script excercise commit 21.draw-pyramid

---
+++ week-01/day-04/21.draw-pyramid.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+let lineCount: number = 12;
+let width: number = lineCount  * 2 - 1;
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+for ( let i:number = 1;i<=lineCount;i++ ){
+    let collector:string = '';
+    for ( let j: number = 1;j<=width;j++ ){
+        if ( j <= lineCount - i || j > width - ( lineCount - i ) ) {
+            collector += ' ';
+        } else {
+            collector += '*';
+        }
+        //console.log(j);
+    }
+    console.log(collector);
+}
