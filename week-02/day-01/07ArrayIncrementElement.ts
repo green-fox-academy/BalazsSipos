5cf59b8f490a5f8b9e90160c00c151f15da86d06
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-01 14:21:32 +0200

    07ArrayIncrementElement

---
+++ week-02/day-01/07ArrayIncrementElement.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+let numlist: number[] = [1, 2, 3, 4, 5];
+// -  Increment the third element simply by accessing it
+numlist[2]++;
+// -  Log the third element to the console
+console.log(numlist[2]);
