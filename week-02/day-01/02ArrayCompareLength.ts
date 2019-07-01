a4e41c14addc2cbcd4975fb00c1ed4a93973fe2f
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-01 12:52:41 +0200

    02ArrayCompareLength

--- week-02/day-01/02ArrayCompareLength.ts
+++ week-02/day-01/02ArrayCompareLength.ts
@@ -5,8 +5,8 @@
 // -  Create a variable named `secondList`
 //    with the following content: `[4, 5]`
 // -  Log "p2 is longer" to the console if `secondList` has more elements than `firstList`
-let firstList: number[] = [1,2,3];
-let secondList: number[] = [4,5];
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
 
 if (secondList.length > firstList.length) {
   console.log("p2 is longer");
