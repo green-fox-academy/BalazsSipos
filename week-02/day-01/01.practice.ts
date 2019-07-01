e119dacb00f2a72f43f650d25f71dfaac695d6a5
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-01 10:59:57 +0200

    Week start excercise initial commit

---
+++ week-02/day-01/01.practice.ts
@@ -0,0 +1,13 @@
+// var arr = ["orange", "mango", "banana", "sugar", "tea"];  
+// var removed = arr.splice(2, 0, "water");  
+// console.log("After adding 1: " + arr );  
+// console.log("removed is: " + removed); 
+          
+// removed = arr.splice(3, 1);  
+// console.log("After removing 1: " + arr );  
+// console.log("removed is: " + removed);
+
+let names: string[] = []
+console.log(names);
+names = ["one", "two", "three"];
+console.log(names);
