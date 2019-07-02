709dfd832c353b36cde245c7933e466b67384ad9
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-02 16:20:54 +0200

    14DataStructuresProductDatabase2

--- week-02/day-02/14DataStructuresProductDatabase1And2.ts
+++ week-02/day-02/14DataStructuresProductDatabase1And2.ts
@@ -49,4 +49,23 @@ if (valuesList.indexOf(125) > 0) {
 // What is the cheapest product?
 console.log('Least expensive is ' + (Object.keys(products)[valuesList.indexOf(Math.min(...valuesList))]));
 
+// Which products cost less than 201? (just the name)
+// Which products cost more than 150? (name + price)
+let cheapProducts: string[] = [];
+let expProducts = {};
+
+Object.keys(products).forEach(function (key) {
+  if (products[key] < 201) {
+    cheapProducts.push(key);
+  }
+  if (products[key] > 150) {
+    expProducts[key] = products[key];
+  }
+});
+console.log('Cheaper, than 201: ' + cheapProducts);
+console.log('Costs more, than 150: ');
+Object.keys(expProducts).forEach(function (key) {
+  console.log(key + ' ' + products[key]);
+});
+
 console.log(products);
