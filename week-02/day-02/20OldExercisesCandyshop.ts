ace3380c21fd5ea648ffbd56a4d806e9f31f801d
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-02 21:21:33 +0200

    20OldExercisesCandyshop

---
+++ week-02/day-02/20OldExercisesCandyshop.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+let shopItems: any[] = ['Cupcake', 2, 'Brownie', false];
+
+// Accidentally we added "2" and "false" to the array.
+// Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+// No, don't just remove the items :)
+// Create a function called sweets() which takes the list as a parameter.
+// Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
+
+function sweets(list: any[]): string[] {
+  list.forEach(function (value, index) {
+    if (value === 2) {
+      list[index] = 'Croissant';
+    }
+    if (value === false) {
+      list[index] = 'Ice cream';
+    }
+  });
+  return list;
+}
+
+console.log(sweets(shopItems));
+export = sweets;
