3dde1d00fe5216d4a3a6b894fd6183e749f68de2
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-03 12:12:30 +0200

    04GoToTheCenter

--- week-02/day-03/04GoToTheCenter.ts
+++ week-02/day-03/04GoToTheCenter.ts
@@ -9,3 +9,15 @@ const ctx = canvas.getContext('2d');
 // The x and y coordinates of the line's starting point
 // and draws a line from that point to the center of the canvas
 // Draw at least 3 lines with that function using a loop.
+function drawALine(x: number, y: number) {
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(canvas.width / 2, canvas.height / 2);
+  ctx.stroke();
+}
+
+drawALine(50, 50);
+drawALine(200, 200);
+drawALine(500, 150);
+drawALine(500, 400);
+drawALine(500, 10);
