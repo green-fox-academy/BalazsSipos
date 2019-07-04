1b6a0ca0b453c03003a20765a0105d502b0bbf2a
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-04 17:33:14 +0200

    30OldExercisesGuessMyNumber

--- week-02/day-02/30NOT_READY_OldExercisesGuessMyNumber.js
+++ week-02/day-02/30NOT_READY_OldExercisesGuessMyNumber.js
@@ -10,17 +10,44 @@ var readline = require('readline').createInterface({
 // })
 var randomNumber = Math.floor(Math.random() * 101);
 console.log(randomNumber);
-readline.question('I\'ve the number between 1-100. You have 5 lives. Try to guess it!', function (guess) {
-    console.log('Your guess is: ' + guess);
-    if (guess == randomNumber) {
-        console.log('Congrat, you guessed it right!');
-    }
-    else if (guess > randomNumber) {
-        console.log('Too high. You have 4 lives left.');
-    }
-    readline.question('What2', function (guess2) {
-        console.log("Hi " + guess2 + "!");
-        readline.close();
+// function read(textToWrite: string) {
+//   readline.question(textToWrite, (inputFromUser) => {
+//   })
+// }
+// readline.question('I\'ve the number between 1-100. You have 5 lives. Try to guess it!', (guess) => {
+//   console.log('Your guess is: ' + guess);
+//   if(guess == randomNumber) {
+//     console.log('Congrat, you guessed it right!');
+//   } else if (guess > randomNumber) {
+//     console.log('Too high. You have 4 lives left.');
+//   }
+//   readline.question('What2', (guess2) => {
+//     console.log(`Hi ${guess2}!`);
+//     readline.close()
+//   })
+// })
+function comm(comment) {
+    var end = false;
+    console.log(end);
+    readline.question(comment, function (guess) {
+        // console.log(randomNumber);
+        console.log('Your guess is: ' + guess);
+        if (guess === randomNumber) {
+            console.log('Congrat, you guessed it right!');
+            end = true;
+            readline.close();
+        }
+        else if (guess > randomNumber) {
+            console.log('Too high. You have 4 lives left.');
+        }
     });
-});
+    console.log(end);
+    if (!end) {
+        readline.question('What2', function (guess2) {
+            console.log("Hi " + guess2 + "!");
+            readline.close();
+        });
+    }
+}
+comm('I\'ve the number between 1-100. You have 5 lives. Try to guess it!');
 var args = process.argv.splice(2); // Just a helper for you to get started
--- week-02/day-02/30NOT_READY_OldExercisesGuessMyNumber.ts
+++ week-02/day-02/30NOT_READY_OldExercisesGuessMyNumber.ts
@@ -14,25 +14,51 @@ const readline = require('readline').createInterface({
 let randomNumber: number = Math.floor(Math.random() * 101);
 console.log(randomNumber);
 
-function read(textToWrite: string) {
-  readline.question(textToWrite, (inputFromUser) => {
+// function read(textToWrite: string) {
+//   readline.question(textToWrite, (inputFromUser) => {
     
-  })
+//   })
+// }
+
+// readline.question('I\'ve the number between 1-100. You have 5 lives. Try to guess it!', (guess) => {
+//   console.log('Your guess is: ' + guess);
+//   if(guess == randomNumber) {
+//     console.log('Congrat, you guessed it right!');
+
+//   } else if (guess > randomNumber) {
+//     console.log('Too high. You have 4 lives left.');
+//   }
+//   readline.question('What2', (guess2) => {
+//     console.log(`Hi ${guess2}!`);
+//     readline.close()
+//   })
+// })
+
+function comm(comment: string) {
+  let end: boolean = false;
+
+  console.log(end);
+  readline.question(comment, (guess) => {
+    // console.log(randomNumber);
+    console.log('Your guess is: ' + guess);
+    if(guess === randomNumber) {
+      console.log('Congrat, you guessed it right!');
+      end = true;
+      readline.close();
+    } else if (guess > randomNumber) {
+      console.log('Too high. You have 4 lives left.');
+    }
+  });
+  console.log(end);
+  if(!end) {
+    readline.question('What2', (guess2) => {
+      console.log(`Hi ${guess2}!`);
+      readline.close()
+    });
+  }
 }
 
-readline.question('I\'ve the number between 1-100. You have 5 lives. Try to guess it!', (guess) => {
-  console.log('Your guess is: ' + guess);
-  if(guess == randomNumber) {
-    console.log('Congrat, you guessed it right!');
-
-  } else if (guess > randomNumber) {
-    console.log('Too high. You have 4 lives left.');
-  }
-  readline.question('What2', (guess2) => {
-    console.log(`Hi ${guess2}!`);
-    readline.close()
-  })
-})
+comm('I\'ve the number between 1-100. You have 5 lives. Try to guess it!');
 
 const args = process.argv.splice(2); // Just a helper for you to get started
 // console.log(args);
