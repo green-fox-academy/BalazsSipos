c2f4f9bb1ed3f1d0b5c3335a314c946aa0713e28
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-05 17:10:45 +0200

    w2d5 05SuperHexagon draw by one hexagon at a time

--- week-02/day-05/05SuperHexagon.ts
+++ week-02/day-05/05SuperHexagon.ts
@@ -6,58 +6,62 @@ const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 let drawlines = {
   numberOfHexagonPerSize: 4,
   widthInTriangles: 0,
-  // triangleSizeLength: 0,
   xStep: 0, // the distance of x coordinates between the lines in px
-  // triangleM: ((this.triangleSizeLength) ** 2 - (this.triangleSizeLength / 2) ** 2) ** 0.5, // the height of the triangle
   yStep: 0, // the distance of y coordinates between the lines in px
   moveDownRight: [],
   moveUpRight: [],
   moveDown: [],
+  startXY: [];
 
   initialize: function () {
     this.widthInTriangles = (this.numberOfHexagonPerSize - 1) * 3 + 2;
-    // this.triangleSizeLength = canvas.width / this.widthInTriangles;
     this.xStep = canvas.width / this.widthInTriangles;
     this.yStep = ((this.xStep) ** 2 - (this.xStep / 2) ** 2) ** 0.5;
     this.moveDownRight = [this.xStep * 1.5, this.yStep];
     this.moveUpRight = [this.xStep * 1.5, this.yStep * (-1)];
+    this.startXY = [((this.numberOfHexagonPerSize - 2) * 1.5 + 2) * this.xStep, 0];
   },
 
-  drawLinesRight: function (repetition: number, startXY: number[], moveType: number[]) {
-    let xStep: number = this.xStep;
-    let yStep: number = this.yStep;
-    for (let i: number = 1; i <= repetition; i++) {
-      ctx.beginPath();
-      ctx.moveTo(startXY[0] + (i - 1) * moveType[0], startXY[1] + (i - 1) * moveType[1]);
-      ctx.lineTo((startXY[0] + xStep / 2) + (i - 1) * moveType[0], (startXY[1] - yStep) + (i - 1) * moveType[1]);
-      ctx.stroke();
+  // Draw a single line based on start and end point
+  drawLine: function (startXY: number[], endXY: number[]): void {
+    ctx.beginPath();
+    ctx.moveTo(startXY[0], startXY[1]);
+    ctx.lineTo(endXY[0], endXY[1]);
+    ctx.stroke();
+  },
+
+  // Draw one hexagon
+  drawHexagon: function (startXY: number[]): void {
+    let pointA: number[] = [startXY[0] + this.xStep, startXY[1]];
+    let pointB: number[] = [startXY[0] + this.xStep * 1.5, startXY[1] + this.yStep];
+    let pointC: number[] = [startXY[0] + this.xStep, startXY[1] + this.yStep * 2];
+    let pointD: number[] = [startXY[0], startXY[1] + this.yStep * 2];
+    let pointE: number[] = [startXY[0] - this.xStep * 0.5, startXY[1] + this.yStep];
+    this.drawLine(startXY, pointA); // -
+    this.drawLine(pointA, pointB); // \
+    this.drawLine(pointB, pointC); // /
+    this.drawLine(pointC, pointD); // -
+    this.drawLine(pointD, pointE); // \
+    this.drawLine(pointE, startXY); // /
+  },
+
+  // Draw a vertical set of hexagons
+  paintDown(startXY: number[], PiecestoPaint: number) {
+    for (let i: number = 1; i <= PiecestoPaint; i++) {
+      this.drawHexagon([startXY[0], startXY[1] + (i - 1) * 2 * this.yStep]);
+    }
+  },
+
+  // Draw all hexagons
+  drawAllHexagons: function () {
+    this.paintDown(this.startXY, this.numberOfHexagonPerSize * 2 - 1);
+    for (let i: number = 1; i < this.numberOfHexagonPerSize; i++) {
+      this.paintDown([this.startXY[0] - 1.5 * i * this.xStep, this.startXY[1] + this.yStep * i], this.numberOfHexagonPerSize * 2 - 1 - i);
+      this.paintDown([this.startXY[0] + 1.5 * i * this.xStep, this.startXY[1] + this.yStep * i], this.numberOfHexagonPerSize * 2 - 1 - i);
     }
   }
-}
 
-// for (let i: number = 1; i <= numberOfHexagonPerSize * 2 - 1; i++) {
-//   drawLinesRight(numberOfHexagonPerSize * 2, [0, 4 * yStep]);
-// }
-// drawlines.drawLinesRight(drawlines['numberOfHexagonPerSize'] * 2, [0, 4 * drawlines['yStep']);
+}
 
 drawlines.initialize();
-
-
-// Right angles
-drawlines.drawLinesRight(drawlines['numberOfHexagonPerSize'] * 2, [0, drawlines['numberOfHexagonPerSize'] * drawlines['yStep']], drawlines['moveDownRight']);
-
-for (let i: number = 1; i <= (drawlines['numberOfHexagonPerSize'] - 1); i++) {
-  drawlines.drawLinesRight(drawlines['numberOfHexagonPerSize'] * 2 - i, [0, (drawlines['numberOfHexagonPerSize'] + i * 2) * drawlines['yStep']], drawlines['moveDownRight']);
-}
-for (let i: number = 1; i <= (drawlines['numberOfHexagonPerSize'] - 1); i++) {
-  drawlines.drawLinesRight(drawlines['numberOfHexagonPerSize'] * 2 - i, [drawlines['xStep'] * 1.5 * i, (drawlines['numberOfHexagonPerSize'] - i) * drawlines['yStep']], drawlines['moveDownRight']);
-}
-
-// Left angles
-drawlines.drawLinesRight(drawlines['numberOfHexagonPerSize'] * 2, [0, (drawlines['numberOfHexagonPerSize']*2+2) * drawlines['yStep']], drawlines['moveUpRight']);
-
-
-// document.write(drawlines['numberOfHexagonPerSize']);
-// document.write(drawlines.widthInTriangles);
-// document.write(drawlines.xStep);
-// document.write(drawlines.yStep);
+drawlines.drawAllHexagons();
