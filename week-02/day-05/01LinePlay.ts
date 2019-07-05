c5327d5c6c356f7ee890d87137260f95f14e7cf5
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-05 10:03:42 +0200

    w2d5 01LinePlay

---
+++ week-02/day-05/01LinePlay.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+let cropAmount: number = 0.2; // the percentage, we want to skip at top left and bottom right (0.2 = 20%)
+let numberOfLines: number = 14;
+let xStep = canvas.width * (1 - cropAmount) / numberOfLines; // the distance of x coordinates between to lines in px
+let yStep = canvas.height * (1 - cropAmount) / numberOfLines;
+
+function drawLines1(color: string, xLength: number, yLength: number) {
+  for (let i: number = 1; i <= numberOfLines; i++) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.moveTo(0, yLength * cropAmount + (i - 1) * yStep);
+    ctx.lineTo(xStep * i, yLength);
+    ctx.stroke();
+  }
+}
+
+function drawLines2(color: string, xLength: number, yLength: number) {
+  for (let i: number = 1; i <= numberOfLines; i++) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.moveTo(xLength*cropAmount + (i-1)*xStep, 0);
+    ctx.lineTo(xLength, i*yStep);
+    ctx.stroke();
+  }
+}
+
+drawLines1('green', canvas.width, canvas.height);
+drawLines2('purple', canvas.width, canvas.height);
