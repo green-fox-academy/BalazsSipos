433c616bde5742c7b2674266c643f5a3514858bd
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-10 12:20:35 +0200

    01 PostIt

---
+++ week-03/day-03/01PostIt/01PostIt.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+// Create a PostIt a class that has
+// a backgroundColor
+// a text on it
+// a textColor
+class PostIt {
+  backgroundColor: string;
+  text: string;
+  textColor: string;
+
+  constructor(bckG: string, txt: string, txtC: string) {
+    this.backgroundColor = bckG;
+    this.text = txt;
+    this.textColor = txtC;
+  }
+}
+
+// Create a few example post-it objects:
+// an orange with blue text: "Idea 1"
+let postIt_1 = new PostIt('orange', 'Idea 1', 'blue');
+// a pink with black text: "Awesome"
+let postIt_2 = new PostIt('pink', 'Awesome', 'black');
+
+// a yellow with green text: "Superb!"
+let postIt_3 = new PostIt('yellow', 'Superb!', 'green');
+
+console.log(postIt_1);
+console.log(postIt_2);
+console.log(postIt_3);
