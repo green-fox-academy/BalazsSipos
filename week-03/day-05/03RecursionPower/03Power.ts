143696d6fb0a1b7311799666326db5e7a8b475fb
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-12 12:32:05 +0200

    03Recursion Power

--- week-03/day-05/03RecursionPower/03Power.ts
+++ week-03/day-05/03RecursionPower/03Power.ts
@@ -2,3 +2,14 @@
 
 // Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
+function powerN(base: number, power: number): number {
+  if (base >= 1 && power >= 1) {
+    if (power <= 1) {
+      return base;
+    } else {
+      return base * powerN(base, power - 1);
+    }
+  }
+}
+
+console.log(powerN(2, 3));
