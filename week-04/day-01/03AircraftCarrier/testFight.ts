c074f662952176f717a53717426b6c692ab39be0
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-15 17:24:17 +0200

    03 AircraftCarrier

--- week-04/day-01/03AircraftCarrier/testFight.ts
+++ week-04/day-01/03AircraftCarrier/testFight.ts
@@ -36,7 +36,7 @@ let carrier2: Carrier = new Carrier();
 // Check empty carrier
 console.log(carrier2.getStatus());
 console.log('--------------------------------------------');
-// Create 4 aircrafts
+// Create 5 aircrafts
 let f163: F16 = new F16();
 let f164: F16 = new F16();
 let f353: F16 = new F35();
@@ -58,6 +58,7 @@ console.log('--------------------------------------------');
 console.log('--------------------------------------------');
 console.log('--------------------------------------------');
 
+// Call fight methods on both carriers
 console.log(carrier1.fight(carrier2));
 console.log(carrier2.fight(carrier1));
 console.log(carrier1.getStatus());
