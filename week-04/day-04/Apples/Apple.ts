62fde67bd5c7fe36bb5b39acfed1db1772d57ed8
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-07-18 12:40:02 +0200

    return apple test

---
+++ week-04/day-04/Apples/Apple.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+class Apple {
+  getApple() {
+    return 'apple';
+  }
+
+  sum(int1: number, int2: number) {
+    return int1 + int2;
+  }
+}
+
+export { Apple };
---
+++ week-04/day-04/Apples/test-apple.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+import { Apple } from './Apple';
+import { test } from 'tape';
+
+test('check getApple\'s result', t => {
+  const apple = new Apple();
+  const actual = apple.getApple();
+  const expected = 'apple';
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('check sum function', t => {
+  const apple = new Apple();
+  t.equal(apple.sum(1,2), 3);
+  t.end();
+});
