3978623cdfad5520282bbcb3a876e6096ca7b276
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-08-06 16:43:56 +0200

    WebController counter is working

--- week-06/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,13 +3,19 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+    AtomicLong atomicLong = new AtomicLong(1);
+
     @GetMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World! (It is a web page.)");
+    public String greeting(Model model, @RequestParam(value="name") String name) {
+        model.addAttribute("name", name);
+        model.addAttribute("greetingCounter", atomicLong.getAndIncrement());
         return "greeting2";
     }
 }
